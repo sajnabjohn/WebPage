aa0f1ca9644ee78216d675a0eea434c35e577c29
sajna.j <sajna.j@teamta.in>
2017-06-12 17:02:54 +0530

    Initial commit

---
+++ Example/WebPage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WebPage
+//
+//  Created by sajna.j on 06/12/2017.
+//  Copyright (c) 2017 sajna.j. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
